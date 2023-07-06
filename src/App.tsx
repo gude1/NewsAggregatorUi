28a3aa314a0b9d34c99120996391c852b4c8576c
Owolabi Gideon <owolabigidslab@gmail.com>
2023-07-06 14:12:14 +0100

    setup routing for auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Signup from "./pages/Signup/Signup";
+import Signin from "./pages/Signin/Signin";
 
-function AuthScreens() {}
+function AuthScreenWrapper() {
+  return (
+    <div className="border-2 border-red-800 h-screen flex">
+      <img className="flex-[1] border-2 border-green-800" />
+      <div className="flex-[1]">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="auth" element={<AuthScreenWrapper />}>
+            <Route path="signup" element={<Signup />} />
+            <Route path="signin" element={<Signin />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
