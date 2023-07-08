459770aef25c55d8a0e5b077cfbb15480249aa90
Owolabi Gideon <owolabigidslab@gmail.com>
2023-07-08 12:48:59 +0100

    created newsitem component

---
+++ src/components/NewsItem/NewsItem.tsx
@@ -0,0 +1,7 @@
+type NewsItemsProps = {
+  className: React.HTMLAttributes<HTMLDivElement>["className"];
+};
+
+export default function NewsItem({}: NewsItemsProps) {
+  return <div className="">NewsItem</div>;
+}
